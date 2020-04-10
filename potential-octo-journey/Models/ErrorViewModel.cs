34b036a2c3d4571548228305f3dd71ce13d3a672
Daniel Gustafsson <daniel.getset@gmail.com>
2020-04-10 08:38:55 +0200

    asp net

---
+++ potential-octo-journey/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace potential_octo_journey.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
